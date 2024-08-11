f33771437bb371ee675a76c81e03189aa4996f47
Jose Suarez <j_adrian_s@outlook.com>
2024-08-11 17:04:39 -0300

    se agrega html sueños y json local

---
+++ js/sueños.js
@@ -0,0 +1,18 @@
+const lista = document.querySelector('#listado')
+
+fetch('/miarchivo.json')
+    .then((res) => res.json())
+    .then((data) => {
+        data.forEach((producto) => {
+            const sueño = document.createElement('div')
+            sueño.innerHTML = `<div class="card" style="width: 18rem;">
+  <div class="card-body">
+    <h5 class="card-title">${producto.numero}</h5>
+    <p class="card-text">${producto.significado}</p>
+    
+  </div>
+</div>`
+    
+            lista.append(sueño)
+        });
+    })
